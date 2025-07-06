784c7df781fe184ba223dffedaabc03f1993a26e
sarthaK <killsw1tch@localhost.localdomain>
2025-07-07 02:46:12 +0500

    Added one more print statement

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,6 +2,7 @@ public class HelloWorld
 {
     public static void main(String args[])
     {
-        print("hello world!");
+        System.out.println("hello world!");
+        System.out.println("Welcome to the Earth");
     }
 }
